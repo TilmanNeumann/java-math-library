a6277c28d8921fce4a909c60d511b0900efc7642
Till <Tilman.Neumann@web.de>
2018-11-19 20:53:37 +0100

    add license header

--- src/de/tilman_neumann/jml/CollatzSequenceTest.java
+++ src/de/tilman_neumann/jml/CollatzSequenceTest.java
@@ -1,3 +1,16 @@
+/*
+ * java-math-library is a Java library focused on number theory, but not necessarily limited to it. It is based on the PSIQS 4.0 factoring project.
+ * Copyright (C) 2018 Tilman Neumann (www.tilman-neumann.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
 package de.tilman_neumann.jml;
 
 import static de.tilman_neumann.jml.base.BigIntConstants.*;
@@ -94,6 +107,7 @@ public class CollatzSequenceTest {
 	/**
 	 * Run the 2n+1 sequence until we find some n that was already contained in the sequence of some smaller n.
 	 */
+	@SuppressWarnings("unused")
 	private static void test3nPlus1RepeatSequence() {
 		TreeSet<Integer> lengths = new TreeSet<>();
 		int maxLength = 0;
--- src/de/tilman_neumann/jml/FermatCatalanConjectureTest.java
+++ src/de/tilman_neumann/jml/FermatCatalanConjectureTest.java
@@ -1,3 +1,16 @@
+/*
+ * java-math-library is a Java library focused on number theory, but not necessarily limited to it. It is based on the PSIQS 4.0 factoring project.
+ * Copyright (C) 2018 Tilman Neumann (www.tilman-neumann.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
 package de.tilman_neumann.jml;
 
 import java.math.BigInteger;
@@ -91,6 +104,7 @@ public class FermatCatalanConjectureTest {
 	 * @param m
 	 * @param n
 	 */
+	@SuppressWarnings("unused")
 	private static void testAssumingBealsConjecture(int m, int n) {
 		// Catalan's conjecture means that the only solution with x=1 or y=1 is 2^3 + 1^n = 3^2 -> no need to test that
 		for (int a=2; ; a++) {
