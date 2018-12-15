14bcda1e42809bffbb459f1c8b80f48d99a4b306
Till <Tilman.Neumann@web.de>
2018-12-15 15:17:34 +0100

    now the step4 term helps at small k

--- src/de/tilman_neumann/jml/factor/lehman/Lehman_TDivLast.java
+++ src/de/tilman_neumann/jml/factor/lehman/Lehman_TDivLast.java
@@ -101,11 +101,11 @@ public class Lehman_TDivLast extends FactorAlgorithmBase {
 				// k even -> make sure aLimit is odd
 				aLimit |= 1;
 				aStep = 2;
-//			} else {
-//				// minor improvement following https://de.wikipedia.org/wiki/Faktorisierungsmethode_von_Lehman
-//				final int m = (int) (((k+N)&3) - (aLimit&3));
-//				aLimit = m<0 ? aLimit + m + 4 : aLimit + m;
-//				aStep = 4;
+			} else {
+				// minor improvement following https://de.wikipedia.org/wiki/Faktorisierungsmethode_von_Lehman
+				final int m = (int) (((k+N)&3) - (aLimit&3));
+				aLimit = m<0 ? aLimit + m + 4 : aLimit + m;
+				aStep = 4;
 			}
 			
 			// processing the a-loop top-down is faster than bottom-up
