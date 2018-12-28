81b11863678aa3ed362920cbda57037f2b7fdb8f
Till <Tilman.Neumann@web.de>
2018-12-28 11:40:30 +0100

    test correctness of BigIntegers modularInverse

--- src/de/tilman_neumann/jml/factor/pollardRho/ModInverseTest.java
+++ src/de/tilman_neumann/jml/factor/pollardRho/ModInverseTest.java
@@ -1,5 +1,8 @@
 package de.tilman_neumann.jml.factor.pollardRho;
 
+import static de.tilman_neumann.jml.base.BigIntConstants.*;
+import static org.junit.Assert.assertEquals;
+
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
@@ -20,12 +23,29 @@ public class ModInverseTest {
 	public static void main(String[] args) {
 		ConfigUtil.initProject();
 		
-		for (int bits=10; bits<63; bits++) {
+		// test absolute random numbers that are invertible
+		for (int bits=10; bits<100; bits++) {
 			LOG.debug("Test " + NCOUNT + " modular inverses of " + bits + " bit numbers...");
 			for (int i=0; i<NCOUNT; i++) {
-				BigInteger N = new BigInteger(bits, RNG);
-				BigInteger mod = new BigInteger(bits, RNG);
+				// test numbers:
+				// mod may be any integer >= 2
+				BigInteger mod;
+				do {
+					mod = new BigInteger(bits, RNG);
+				} while (mod.compareTo(I_1)<=0);
+				// we need N >= 2, N odd and gcd(N,mod)==1
+				BigInteger N, gcd;
+				boolean invertible;
+				do {
+					N = new BigInteger(bits, RNG);
+					gcd = N.gcd(mod);
+					invertible = gcd.compareTo(I_1)==0;
+				} while (N.compareTo(I_1)<=0 || N.and(I_1).equals(I_0) || !invertible);
+				LOG.debug("N=" + N + ", mod=" + mod + ", gcd=" + gcd);
+				
+				// compute and check inverse
 				BigInteger inv = N.modInverse(mod);
+				assertEquals(I_1, N.multiply(inv).mod(mod));
 			}
 		}
 	}
