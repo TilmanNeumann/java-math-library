92425a5cb59d399c3af7abcd2002ffb9cc18eb5b
Till <Tilman.Neumann@web.de>
2018-12-30 17:33:24 +0100

    added header +++ renamed constants

--- src/de/tilman_neumann/jml/factor/pollardRho/PollardRhoBrentMontgomery63.java
+++ src/de/tilman_neumann/jml/factor/pollardRho/PollardRhoBrentMontgomery63.java
@@ -1,3 +1,16 @@
+/*
+ * java-math-library is a Java library focused on number theory, but not necessarily limited to it. It is based on the PSIQS 4.0 factoring project.
+ * Copyright (C) 2018 Tilman Neumann (www.tilman-neumann.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
 package de.tilman_neumann.jml.factor.pollardRho;
 
 import static org.junit.Assert.assertTrue;
@@ -19,8 +32,10 @@ import de.tilman_neumann.util.SortedMultiset;
 /**
  * Brents's improvement of Pollard's Rho algorithm using Montgomery multiplication.
  * 
- * The main reason why Montgomery multiplication could be helpful is that no conversions to/from Montgomery form are required.
- * This implementation is long-based and uses the Montgomery reducer R=2^63. Pretty fast.
+ * The main reason why Montgomery multiplication is helpful for Pollard-Rho is that
+ * no conversions to/from Montgomery form are required.
+ * 
+ * This implementation is long-based and uses the Montgomery reducer R=2^63. Already quite fast.
  * 
  * @see [Richard P. Brent: An improved Monte Carlo Factorization Algorithm, 1980]
  * @see [http://projecteuler.chat/viewtopic.php?t=3776]
@@ -36,9 +51,9 @@ public class PollardRhoBrentMontgomery63 extends FactorAlgorithmBase {
 
 	// Reducer constants
 	private static final long R = 1L << 63; // The reducer, a power of 2
-	private static final int Rbits = 63;
-	private static final long RMask = ~R; // R-1 = 0x7FFFFFFFFFFFFFFFL, helps to compute x mod R = x & (R - 1)
-	private static final long Rhalf = 1L << 62;
+	private static final int R_BITS = 63;
+	private static final long R_MASK = ~R; // R-1 = 0x7FFFFFFFFFFFFFFFL, helps to compute x mod R = x & (R - 1)
+	private static final long R_HALF = 1L << 62;
 
 	private long N;
 
@@ -119,7 +134,7 @@ public class PollardRhoBrentMontgomery63 extends FactorAlgorithmBase {
 	 */
 	private void setUpMontgomeryMult() {
 		// initialization
-	    long a = Rhalf;
+	    long a = R_HALF;
 	    long u = 1;
 	    long v = 0;
 	    
@@ -130,7 +145,7 @@ public class PollardRhoBrentMontgomery63 extends FactorAlgorithmBase {
 	            u = u >>> 1; v = v >>> 1;
 	        } else {
 	            u = ((u ^ N) >>> 1) + (u & N);
-	            v = (v >>> 1) + Rhalf;
+	            v = (v >>> 1) + R_HALF;
 	        }
 	    }
 
@@ -149,8 +164,8 @@ public class PollardRhoBrentMontgomery63 extends FactorAlgorithmBase {
 	private long montgomeryMult(final long a, final long b) {
 		Int127 ab = Int127.mul64(a, b);
 		// t = ab * (-1/N) mod R
-		long t = Int127.mul64(ab.and(RMask), minusNInvModR).and(RMask);
-		long reduced = ab.add(Int127.mul64(t, N)).shiftRight(Rbits).getLow();
+		long t = Int127.mul64(ab.and(R_MASK), minusNInvModR).and(R_MASK);
+		long reduced = ab.add(Int127.mul64(t, N)).shiftRight(R_BITS).getLow();
 		long result = reduced<N ? reduced : reduced-N;
 		
 		if (DEBUG) {
