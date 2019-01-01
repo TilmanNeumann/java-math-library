23ecac3758e662c48dc3a6889d35a667f6c421d2
Till <Tilman.Neumann@web.de>
2019-01-01 12:32:45 +0100

    The moebius function is not defined for n=0 +++ clean up

--- src/de/tilman_neumann/jml/MoebiusFunction.java
+++ src/de/tilman_neumann/jml/MoebiusFunction.java
@@ -14,13 +14,12 @@
 package de.tilman_neumann.jml;
 
 import java.math.BigInteger;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 
 import de.tilman_neumann.jml.factor.FactorAlgorithm;
-import de.tilman_neumann.jml.roots.SqrtInt;
 import de.tilman_neumann.util.ConfigUtil;
 import de.tilman_neumann.util.SortedMultiset;
 
@@ -47,10 +46,13 @@ public class MoebiusFunction {
 	 * 0,      if k has repeated prime factors.
 	 * 
 	 * @param n Argument
-	 * @return the moebius function.
+	 * @return moebius(n)
 	 */
 	public static int moebius(BigInteger n) {
-		if (n.compareTo(I_1) <= 0) {
+		if (n.signum()<1) {
+			throw new IllegalArgumentException("n=" + n + ", but the moebius function supports n>=1 only");
+		}
+		if (n.equals(I_1)) {
 			return 1;
 		}
 		
@@ -59,7 +61,7 @@ public class MoebiusFunction {
 		//LOG.debug("factors of " + n + " = " + factors);
 		
 		if (factors==null || factors.keyCount()==0) {
-			// prime
+			// n is prime
 			return -1;
 		}
 		
@@ -67,67 +69,16 @@ public class MoebiusFunction {
 		int k = 1;
 		for (Map.Entry<BigInteger, Integer> factorAndMultiplicity : factors.entrySet()) {
 			final int e = factorAndMultiplicity.getValue().intValue();
-			if ( e > 1 ) {
+			if (e > 1) {
 				return 0;
 			}
-			if ( e == 1) {
-				k *= -1 ;
+			if (e == 1) {
+				k = -k;
 			}
 		}
 		// return (-1)^k:
 		return k;
 	}
-
-	  //-------------------------------------------------------------------------
-	  // This returns the value of the Moebius function, mu(x). See
-	  // http://planetmath.org/encyclopedia/MoebiusFunction.html for a careful
-	  // definition.
-	  //
-	  // The algorithm used is fairly efficient.
-	  //
-	  // Adapted from Natura math Functions.
-	  //-------------------------------------------------------------------------
-
-	  public static BigInteger moebius_natura(BigInteger x)
-	  {
-	    if(x.equals(new BigInteger("1")))
-	    {
-	      return new BigInteger("1");
-	    }
-	    else
-	    {
-	      if((isSquareFree(x)).equals(new BigInteger("0")))
-	      {
-	        return new BigInteger("0");
-	      }
-	      else
-	      { 
-	    	  SortedMultiset<BigInteger> factors = FactorAlgorithm.DEFAULT.factor(x);
-	    	  return new BigInteger("-1").pow(factors.totalCount());
-	      }
-	    }
-	  }
-
-	  //-------------------------------------------------------------------------
-	  // This functions returns 1 if no square divides x, 0 otherwise.
-	  // I am not aware of any more efficient algorithm.
-	  //
-	  // Adapted from Natura math Functions.
-	  //-------------------------------------------------------------------------
-
-	  public static BigInteger isSquareFree(BigInteger x)
-	  {
-	    BigInteger current = new BigInteger("2");
-
-	    for(; current.compareTo(SqrtInt.iSqrt(x)[0])<=0; current = current.add(new BigInteger("1"))) {
-	      if(x.mod(current.multiply(current)).equals(new BigInteger("0")))
-	      {
-	        return new BigInteger("0");
-	      }
-	    }
-
-	    return new BigInteger("1");
-	  }
 
 	/**
 	 * Tests.
@@ -137,17 +88,12 @@ public class MoebiusFunction {
 	public static void main(String[] args) {
     	ConfigUtil.initProject();
     	int nMax = 100;
-		int[] myResults = new int[nMax];
-		int[] naturaResults = new int[nMax];
-		for (int n=0; n<nMax; n++) {
+		ArrayList<Integer> results = new ArrayList<>();
+		for (int n=1; n<nMax; n++) {
 			BigInteger bigN = BigInteger.valueOf(n);
-			myResults[n] = moebius(bigN);
-			naturaResults[n] = moebius_natura(bigN).intValue();
+			results.add(moebius(bigN));
 		}
-		LOG.info("my Moebius function = " + Arrays.toString(myResults));
-		LOG.info("Natura Moebius fct. = " + Arrays.toString(naturaResults));
-		// Result: The two implementations are equal except for n=0,
-		// where my implementation gives 1 and natura -1.
-		// Mupad does not allow an input of n==0...
+		LOG.info("Moebius function = " + results);
+		// Result: 1, -1, -1, 0, -1, 1, -1, 0, 0, 1, -1, 0, -1, 1, 1, 0, -1, 0, -1, 0, 1, 1, -1, 0, 0, ...
 	}
 }
