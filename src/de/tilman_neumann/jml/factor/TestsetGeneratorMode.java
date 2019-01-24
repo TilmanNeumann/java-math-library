8dbd651309a562cfc0075721a1aab5394c8cd638
Till <Tilman.Neumann@web.de>
2019-01-24 17:24:03 +0100

    preparation for more powerful TestsetGenerator

---
+++ src/de/tilman_neumann/jml/factor/TestsetGeneratorMode.java
@@ -0,0 +1,30 @@
+/*
+ * java-math-library is a Java library focused on number theory, but not necessarily limited to it. It is based on the PSIQS 4.0 factoring project.
+ * Copyright (C) 2018 Tilman Neumann (www.tilman-neumann.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
+package de.tilman_neumann.jml.factor;
+
+/**
+ * Definition of the "nature" of test numbers.
+ * @author Tilman Neumann
+ */
+public enum TestsetGeneratorMode {
+	/** Arbitrary random numbers chosen from a certain bit length */
+	RANDOM,
+	
+	/**
+	 * Semiprimes N=a*b with bitLength(min(a,b)) >= bitLength(N)/3 bits.
+	 * Sometimes we'll get min(a,b) <= cbrt(N),
+	 * thus e.g. in the Lehman algorithm, some numbers require trial division.
+	 */
+	MODERATE_SEMIPRIMES
+}
