efbb97dddb48eccafe6e93b13f5a62fde5f44b57
Till <Tilman.Neumann@web.de>
2019-03-03 13:33:11 +0100

    test moderate semiprimes

--- src/de/tilman_neumann/jml/factor/lehman/Lehman_AnalyzeKStructure.java
+++ src/de/tilman_neumann/jml/factor/lehman/Lehman_AnalyzeKStructure.java
@@ -144,7 +144,7 @@ public class Lehman_AnalyzeKStructure extends FactorAlgorithm {
 		// test from 30 to 39 bits
 		for (arrayIndex=0; arrayIndex<10; arrayIndex++) {
 			int bits = arrayIndex+30;
-			BigInteger[] testNumbers = TestsetGenerator.generate(N_COUNT, bits, TestNumberNature.RANDOM_ODD_COMPOSITES);
+			BigInteger[] testNumbers = TestsetGenerator.generate(N_COUNT, bits, TestNumberNature.MODERATE_SEMIPRIMES);
 			LOG.info("Test N having " + bits + " bit");
 			for (BigInteger N : testNumbers) {
 				this.findSingleFactor(N);
