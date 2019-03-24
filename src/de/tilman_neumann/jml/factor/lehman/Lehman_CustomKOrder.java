7d93f8b0477b85eab057c3590effa877985d110f
Till <Tilman.Neumann@web.de>
2019-03-24 17:36:47 +0100

    comment

--- src/de/tilman_neumann/jml/factor/lehman/Lehman_CustomKOrder.java
+++ src/de/tilman_neumann/jml/factor/lehman/Lehman_CustomKOrder.java
@@ -205,7 +205,7 @@ public class Lehman_CustomKOrder extends FactorAlgorithm {
 			}
 		}
 
-		// big k: use improved congruences congruences a == (k*N) (mod 2^s)
+		// big k: use improved congruences a == (k*N) (mod 2^s)
 		for ( ; (k = kArray[i])<kLimit; i++) { // XXX may throw ArrayIndexOutOfBoundsException if N or kLimit are too big
 			long kN = k*N;
 			long a = (long) (sqrt4N * sqrts[i] + ROUND_UP_DOUBLE);
