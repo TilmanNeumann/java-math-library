51c6899827911d4702aaa5c8d155bacad4b36bb2
Till <Tilman.Neumann@web.de>
2019-07-17 09:37:34 +0200

    added Hart_Fast2Mult

--- src/de/tilman_neumann/jml/factor/FactorizerTest.java
+++ src/de/tilman_neumann/jml/factor/FactorizerTest.java
@@ -57,7 +57,7 @@ public class FactorizerTest {
 	
 	// algorithm options
 	/** number of test numbers */
-	private static final int N_COUNT = 1000000;
+	private static final int N_COUNT = 100000;
 	/** the bit size of N to start with */
 	private static final int START_BITS = 20;
 	/** the increment in bit size from test set to test set */
@@ -93,6 +93,7 @@ public class FactorizerTest {
 			new Hart_TDiv_Race(), // good for any kind of numbers < 50 bit
 			new Hart_TDiv_Race2(), // good for semiprimes >= 45 bit, but fails for some N having small factors
 			new Hart_Squarefree(false),
+			new Hart_Fast2Mult(false),
 			
 			// Lehman
 			//new Lehman_Simple(false),
