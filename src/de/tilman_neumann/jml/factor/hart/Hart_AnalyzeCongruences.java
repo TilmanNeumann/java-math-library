1220fec24c5b32e5028fce7cc6e4427ff048263e
Till <Tilman.Neumann@web.de>
2019-11-09 14:54:13 +0100

    analysis without multiplier is a special case of analysis with multipliers -> drop simple implementation

--- src/de/tilman_neumann/jml/factor/hart/Hart_AnalyzeCongruences.java
+++ src/de/tilman_neumann/jml/factor/hart/Hart_AnalyzeCongruences.java
@@ -13,7 +13,7 @@
  */
 package de.tilman_neumann.jml.factor.hart;
 
-import static de.tilman_neumann.jml.base.BigIntConstants.I_1;
+import static de.tilman_neumann.jml.base.BigIntConstants.*;
 
 import java.math.BigInteger;
 import java.util.Arrays;
@@ -26,14 +26,13 @@ import de.tilman_neumann.jml.gcd.Gcd63;
 import de.tilman_neumann.util.ConfigUtil;
 
 /**
- * Analyze the congruences best matching Hart's one-line factor algorithm when tested with 4kN values.
+ * Analyze the congruences best matching Hart's one-line factor algorithm when tested with 4kN values,
+ * where k are multiples of some K_MULT.
+ * 
  * @author Tilman Neumann
  */
 public class Hart_AnalyzeCongruences {
 	private static final Logger LOG = Logger.getLogger(Hart_AnalyzeCongruences.class);
-	
-	/** Use congruences a==kN mod 2^s if true, congruences a==(k+N) mod 2^s if false */
-	private static final boolean USE_kN_CONGRUENCES = true;
 
 	// algorithm options
 	/** number of test numbers */
@@ -45,12 +44,24 @@ public class Hart_AnalyzeCongruences {
 	/** maximum number of bits to test (no maximum if null) */
 	private static final Integer MAX_BITS = 63;
 
-	/** This is a constant that is below 1 for rounding up double values to long. */
-	private static final double ROUND_UP_DOUBLE = 0.9999999665;
+	/** Use congruences a==kN mod 2^s if true, congruences a==(k+N) mod 2^s if false */
+	private static final boolean USE_kN_CONGRUENCES = true;
+	
+	/**
+	 * We only test k-values that are multiples of this constant.
+	 * Best values for performance are 315, 45, 105, 15 and 3, in that order.
+	 */
+	private static final int K_MULT = 3*3*5*7; // 315
 
 	private static final int KMOD = 6;
 	private static final int KNMOD = 6;
 	private static final int AMOD = 6;
+
+	/** This is a constant that is below 1 for rounding up double values to long. */
+	private static final double ROUND_UP_DOUBLE = 0.9999999665;
+	
+	/** Size of arrays */
+	private static final int I_MAX = 1<<20;
 
 	private double[] sqrt;
 
@@ -60,37 +71,47 @@ public class Hart_AnalyzeCongruences {
 	private int[][][][] counts;
 	
 	public Hart_AnalyzeCongruences() {
-		// Precompute sqrts...
-		final int kMax = 1<<25;
-		sqrt = new double[kMax + 1];
-		for (int i = 1; i < sqrt.length; i++) {
-			final double sqrtI = Math.sqrt(i);
-			sqrt[i] = sqrtI;
+		// Precompute sqrts for all k < I_MAX
+		sqrt = new double[I_MAX];
+		for (int i=1; i<I_MAX; i++) {
+			sqrt[i] = Math.sqrt(i*K_MULT);
 		}
+	}
+	
+	public String getName() {
+		return "Hart_AnalyzeCongruencesWithMultiplier";
 	}
 
 	public void findSingleFactor(long N) {
 		long fourN = N<<2;
 		double sqrt4N = Math.sqrt(fourN);
-		int kLimit = (int) Math.cbrt(N);
-		for (int k = 1; k<kLimit; k++) {
-			final long a0 = (long) (sqrt4N * sqrt[k] + ROUND_UP_DOUBLE);
-			for (int adjust=0; adjust<AMOD; adjust++) {
-				long a = a0 + adjust;
-				final long test = a*a - k * fourN;
-				final long b = (long) Math.sqrt(test);
-				if (b*b == test) {
-					long gcd = gcdEngine.gcd(a+b, N);
-					if (gcd>1 && gcd<N) {
-						if (USE_kN_CONGRUENCES) {
-							counts[k%KMOD][(int)((k*N)%KNMOD)][(int)(a0%AMOD)][adjust]++;
-						} else {
-							counts[k%KMOD][(int)((k+N)%KNMOD)][(int)(a0%AMOD)][adjust]++;
+		long a, b, test, gcd;
+		int k = K_MULT;
+		try {
+			for (int i=1; ;) {
+				final long a0 = (long) (sqrt4N * sqrt[i++] + ROUND_UP_DOUBLE);
+				for (int adjust=0; adjust<AMOD; adjust++) {
+					a = a0 + adjust;
+					test = a*a - k * fourN;
+					b = (long) Math.sqrt(test);
+					if (b*b == test) {
+						gcd = gcdEngine.gcd(a+b, N);
+						if (gcd>1 && gcd<N) {
+							if (USE_kN_CONGRUENCES) {
+								counts[k%KMOD][(int)((k*N)%KNMOD)][(int)(a0%AMOD)][adjust]++;
+							} else {
+								counts[k%KMOD][(int)((k+N)%KNMOD)][(int)(a0%AMOD)][adjust]++;
+							}
+							return; // removes the blur at even k!
 						}
-						return; // removes the blur at even k!
 					}
 				}
+				k += K_MULT;
 			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			// this may happen if this implementation is tested with doTDivFirst==false and N having
+			// very small factors, or if N is too big
+			return;
 		}
 	}
 	
