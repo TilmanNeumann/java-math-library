7dc43537f559cdf1917e486ed087fb11f082496a
Till <Tilman.Neumann@web.de>
2020-09-23 19:43:55 +0200

    count quadratic residues from even base values -> interesting!

--- src/de/tilman_neumann/jml/modular/JacobiSymbol.java
+++ src/de/tilman_neumann/jml/modular/JacobiSymbol.java
@@ -375,12 +375,25 @@ public class JacobiSymbol {
      */
     public static TreeSet<Long> getQuadraticResidues(long m) {
     	TreeSet<Long> quadraticResidues = new TreeSet<Long>();
-    	for (long i=0; i<=m/2; i++) {
+    	for (long i=1; i<=m/2; i++) {
     		quadraticResidues.add(i*i % m);
     	}
     	return quadraticResidues;
     }
     
+    /**
+     * Get the quadratic residues of even "i" modulo m.
+     * @param m
+     * @return
+     */
+    public static TreeSet<Long> getEvenQuadraticResidues(long m) {
+    	TreeSet<Long> quadraticResidues = new TreeSet<Long>();
+    	for (long i=0; i<=m/2; i+=2) {
+    		quadraticResidues.add(i*i % m);
+    	}
+    	return quadraticResidues;
+    }
+
 	/**
 	 * Test.
 	 * @param args ignored
@@ -391,13 +404,23 @@ public class JacobiSymbol {
 		LOG.info("m = 100 has " + quadraticResiduesMod100.size() + " quadratic residues: " + quadraticResiduesMod100);
 		
 		ArrayList<Integer> counts = new ArrayList<Integer>();
+		ArrayList<Integer> evenCounts = new ArrayList<Integer>();
+		
 		for (int n=0; n<20; n++) {
 			int m = 1<<n;
+			
 			TreeSet<Long> quadraticResiduesMod2PowN = getQuadraticResidues(m);
 			LOG.info("m = " + m + " has " + quadraticResiduesMod2PowN.size() + " quadratic residues: " + quadraticResiduesMod2PowN);
 			counts.add(quadraticResiduesMod2PowN.size());
+
+			TreeSet<Long> evenQuadraticResiduesMod2PowN = getEvenQuadraticResidues(m);
+			LOG.info("m = " + m + " has " + evenQuadraticResiduesMod2PowN.size() + " 'even' quadratic residues: " + evenQuadraticResiduesMod2PowN);
+			evenCounts.add(evenQuadraticResiduesMod2PowN.size());
 		}
+		
 		LOG.info("counts = " + counts);
 		// A023105(n) = 1, 2, 2, 3, 4, 7, 12, 23, 44, 87, 172, 343, ...
+		LOG.info("evenCounts = " + evenCounts);
+		// a(n) = {1, 1} + A023105(n-2) = 1, 1, 1, 2, 2, 3, 4, 7, 12, 23, 44, 87, 172, 343, 684, 1367, 2732, 5463, 10924, 21847
 	}
 }
