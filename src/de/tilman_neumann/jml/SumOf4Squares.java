26a0c881b339ae90cbee7733d2931e08b84e8930
Till <Tilman.Neumann@web.de>
2020-10-06 16:34:49 +0200

    added some computations of quadratic residues modulo prime powers

--- src/de/tilman_neumann/jml/SumOf4Squares.java
+++ src/de/tilman_neumann/jml/SumOf4Squares.java
@@ -11,7 +11,7 @@
  * You should have received a copy of the GNU General Public License along with this program;
  * if not, see <http://www.gnu.org/licenses/>.
  */
-package de.tilman_neumann.jml.modular;
+package de.tilman_neumann.jml;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,6 +20,8 @@ import java.util.TreeSet;
 
 import org.apache.log4j.Logger;
 
+import de.tilman_neumann.jml.quadraticResidues.QuadraticResidues;
+import de.tilman_neumann.jml.quadraticResidues.QuadraticResiduesMod2PowN;
 import de.tilman_neumann.util.ConfigUtil;
 
 /**
@@ -31,7 +33,7 @@ public class SumOf4Squares {
 
 	private static final Logger LOG = Logger.getLogger(SumOf4Squares.class);
 
-	private static final boolean DEBUG = false;
+	private static final boolean SHOW_ELEMENTS = true;
 	
 	/**
 	 * Compute all elements of A004215 below m, i.e. all k<m such that k can be expressed as a sum of 4 squares
@@ -67,40 +69,28 @@ public class SumOf4Squares {
 	 * @param n
 	 * @return A004215 entries < 2^n
 	 */
-	public static List<Long> getA004215_v2(long n) {
-		if (n < 3) return new ArrayList<>();
+	public static TreeSet<Long> getA004215_v2(int n) {
+		if (n < 3) return new TreeSet<>();
 		
-		List<Long> list = Arrays.asList(new Long[] {0L, 1L}); // mod=2
-		for (int i=1; i<n; i++) {
-			boolean iOdd = (i & 1) == 1;
-			int mod = 1<<i;
-			List<Long> nextList = new ArrayList<>(list); // copy
-			for (long elem : list) {
-				nextList.add(elem + mod);
-			}
-			// remove some stuff
-//			if (DEBUG) LOG.debug("mod=" + mod + ", i odd = " + iOdd + ", list = " + list + ", nextList = " + nextList);
-			if (iOdd) {
-				nextList.remove(Long.valueOf(mod));
-				nextList.remove(Long.valueOf(mod>>1));
-			} else {
-				nextList.remove(Long.valueOf((mod>>1) + (mod>>2)));
-			}
-//			if (DEBUG) LOG.debug("reduced nextList = " + nextList);
-			list = nextList;
+		List<Long> list = QuadraticResiduesMod2PowN.getQuadraticResiduesMod2PowN(n);
+		
+		long m = 1L << n;
+		TreeSet<Long> complement = new TreeSet<>();
+		for (long elem : list) {
+			complement.add(m - elem);
 		}
 		
-		// remove more stuff
-		list.remove(0L);
+		// remove the entries not needed for A004215
+		complement.remove(m);
 		boolean nOdd = (n & 1) == 1;
 		if (nOdd) {
-			list.remove(Long.valueOf(1 << (n-1)));
+			complement.remove(Long.valueOf(1 << (n-1)));
 		} else {
-			list.remove(Long.valueOf((1 << (n-1)) + (1 << (n-2))));
+			complement.remove(Long.valueOf((1 << (n-1)) + (1 << (n-2))));
 		}
-		return list;
+		return complement;
 	}
-	
+
 	/**
 	 * Another implementation using arrays, much faster than the previous ones.
 	 * 
@@ -108,61 +98,29 @@ public class SumOf4Squares {
 	 * @param array an array big enough to take roughly 2^n/6 values
 	 * @return number of entries
 	 */
-	public static int getA004215_v3(long n, long[] array) {
+	public static int getA004215_v3(int n, long[] array) {
 		if (n < 3) return 0;
 		
-		// start with entries mod 2
-		array[0] = 0;
-		array[1] = 1;
-		int count = 2;
+		long[] residues = new long[array.length];
+		int count = QuadraticResiduesMod2PowN.getQuadraticResiduesMod2PowN(n, residues);
 		
-		for (int i=1; i<n; i++) {
-			boolean iOdd = (i & 1) == 1;
-			int mod = 1<<i;
-			
-			// duplicate
-			for (int j=0; j<count; j++) {
-				array [j+count] = array[j] + mod;
-			}
-			count <<= 1;
-			
-			if (DEBUG) LOG.debug("mod=" + mod + ", i odd = " + iOdd + ", count = " + count + ", array = " + Arrays.toString(array));
-			
-			// remove some stuff
-			int nextCount = 0;
-			if (iOdd) {
-				for (int j=0; j<count; j++) {
-					long elem = array[j];
-					if (elem != mod && elem != (mod>>1)) {
-						// if (DEBUG) LOG.debug("mod=" + mod + ", i odd = " + iOdd + ": add elem = " + elem);
-						array [nextCount++] = elem;
-					}
-				}
-			} else {
-				for (int j=0; j<count; j++) {
-					long elem = array[j];
-					if (elem != (mod>>1) + (mod>>2)) {
-						// if (DEBUG) LOG.debug("mod=" + mod + ", i odd = " + iOdd + ": add elem = " + elem);
-						array [nextCount++] = elem;
-					}
-				}
-			}
-			if (DEBUG) LOG.debug("reduced array = " + Arrays.toString(array));
-			count = nextCount;
+		long m = 1L<<n;
+		for (int i=0; i<count; i++) {
+			array[i] = m - residues[count-i-1];
 		}
 		
-		// remove last stuff
+		// remove the entries not needed for A004215
 		int outCount = 0;
 		boolean nOdd = (n & 1) == 1;
 		if (nOdd) {
-			for (int j=1; j<count; j++) { // remove 0
+			for (int j=0; j<count-1; j++) { // remove m
 				long elem = array[j];
 				if (elem != (1L << (n-1))) {
 					array [outCount++] = elem;
 				}
 			}
 		} else {
-			for (int j=1; j<count; j++) { // remove 0
+			for (int j=0; j<count-1; j++) { // remove m
 				long elem = array[j];
 				if (elem != (1 << (n-1)) + (1 << (n-2))) {
 					array [outCount++] = elem;
@@ -179,48 +137,59 @@ public class SumOf4Squares {
 	 * 
 	 * @param args ignored
 	 */
+	// TODO split into more specific tests
 	public static void main(String[] args) {
 		ConfigUtil.initProject();
 		
 		ArrayList<Integer> quadraticResidueCounts = new ArrayList<Integer>();
+		ArrayList<Integer> quadraticResidueCounts_v2 = new ArrayList<Integer>();
+		
 		ArrayList<Integer> a004215EntryCounts = new ArrayList<Integer>();
 		ArrayList<Integer> a004215EntryCounts_v2 = new ArrayList<Integer>();
 		ArrayList<Integer> a004215EntryCounts_v3 = new ArrayList<Integer>();
 		
-		for (int n=0; n<=30; n++) {
+		for (int n=0; n<=10; n++) {
+			LOG.info("n = " + n + ":");
+			
 			long m = 1L<<n;
 			long t0 = System.currentTimeMillis();
 			TreeSet<Long> quadraticResiduesMod2PowN = QuadraticResidues.getQuadraticResidues(m);
 			long t1 = System.currentTimeMillis();
-			LOG.info("n=" + n + ": There are " + quadraticResiduesMod2PowN.size() + " quadratic residues % " + m + (DEBUG ? ": " + quadraticResiduesMod2PowN : "") + " -- duration: " + (t1-t0) + "ms");
+			LOG.info("v1: There are " + quadraticResiduesMod2PowN.size() + " quadratic residues % " + m + (SHOW_ELEMENTS ? ": " + quadraticResiduesMod2PowN : "") + " -- duration: " + (t1-t0) + "ms");
 			quadraticResidueCounts.add(quadraticResiduesMod2PowN.size());
-			
+
+			t0 = System.currentTimeMillis();
+			List<Long> quadraticResiduesMod2PowN_v2 = QuadraticResiduesMod2PowN.getQuadraticResiduesMod2PowN(n);
+			t1 = System.currentTimeMillis();
+			LOG.info("v2: There are " + quadraticResiduesMod2PowN_v2.size() + " quadratic residues % " + m + (SHOW_ELEMENTS ? ": " + quadraticResiduesMod2PowN_v2 : "") + " -- duration: " + (t1-t0) + "ms");
+			quadraticResidueCounts_v2.add(quadraticResiduesMod2PowN_v2.size());
+
 			t0 = System.currentTimeMillis();
 			TreeSet<Long> a004215Entries = getA004215(m);
 			t1 = System.currentTimeMillis();
-			LOG.info("v1: There are " + a004215Entries.size() + " A004215 entries < " + m + (DEBUG ? ": " + a004215Entries : "") + " -- duration: " + (t1-t0) + "ms");
+			LOG.info("v1: There are " + a004215Entries.size() + " A004215 entries < " + m + (SHOW_ELEMENTS ? ": " + a004215Entries : "") + " -- duration: " + (t1-t0) + "ms");
 			a004215EntryCounts.add(a004215Entries.size());
 			
 			t0 = System.currentTimeMillis();
-			List<Long> a004215Entries_v2 = getA004215_v2(n);
+			TreeSet<Long> a004215Entries_v2 = getA004215_v2(n);
 			t1 = System.currentTimeMillis();
-			LOG.info("v2: There are " + a004215Entries_v2.size() + " A004215 entries < " + m + (DEBUG ? ": " + a004215Entries_v2 : "") + " -- duration: " + (t1-t0) + "ms");
+			LOG.info("v2: There are " + a004215Entries_v2.size() + " A004215 entries < " + m + (SHOW_ELEMENTS ? ": " + a004215Entries_v2 : "") + " -- duration: " + (t1-t0) + "ms");
 			a004215EntryCounts_v2.add(a004215Entries_v2.size());
 			
 			t0 = System.currentTimeMillis();
-			long[] a004215Entries_v3 = new long[((1<<n) + 32) / 6]; // #{A004215(k) | k<m} is always near to m/6
-			// 32 is fine up to 
+			long[] a004215Entries_v3 = new long[((1<<n) / 6) + 4]; // #{A004215(k) | k<m} is always near to m/6
 			int count = getA004215_v3(n, a004215Entries_v3);
 			t1 = System.currentTimeMillis();
-			LOG.info("v3: There are " + count + " A004215 entries < " + m + (DEBUG ? ": " + Arrays.toString(a004215Entries_v3) : "") + " -- duration: " + (t1-t0) + "ms");
+			LOG.info("v3: There are " + count + " A004215 entries < " + m + (SHOW_ELEMENTS ? ": " + Arrays.toString(a004215Entries_v3) : "") + " -- duration: " + (t1-t0) + "ms");
 			a004215EntryCounts_v3.add(count);
 			
 			LOG.info("");
 		}
 		
-		LOG.info("quadraticResidueCounts = " + quadraticResidueCounts);
 		// A023105(n) = 1, 2, 2, 3, 4, 7, 12, 23, 44, 87, 172, 343, ...
-		
+		LOG.info("v1: quadraticResidueCounts = " + quadraticResidueCounts);
+		LOG.info("v2: quadraticResidueCounts = " + quadraticResidueCounts_v2);
+
 		LOG.info("v1: a004215EntryCounts = " + a004215EntryCounts);
 		LOG.info("v2: a004215EntryCounts = " + a004215EntryCounts_v2);
 		LOG.info("v3: a004215EntryCounts = " + a004215EntryCounts_v3);
--- src/de/tilman_neumann/jml/quadraticResidues/QuadraticResiduesMod2PowNTest01.java
+++ src/de/tilman_neumann/jml/quadraticResidues/QuadraticResiduesMod2PowNTest01.java
@@ -11,7 +11,7 @@
  * You should have received a copy of the GNU General Public License along with this program;
  * if not, see <http://www.gnu.org/licenses/>.
  */
-package de.tilman_neumann.jml.modular;
+package de.tilman_neumann.jml.quadraticResidues;
 
 import java.util.ArrayList;
 import java.util.TreeSet;
@@ -21,49 +21,21 @@ import org.apache.log4j.Logger;
 import de.tilman_neumann.util.ConfigUtil;
 
 /**
- * Methods to generate quadratic residues or test for quadratic residuosity.
+ * Tests of quadratic residue computations modulo general m.
  * 
  * @author Tilman Neumann
  */
-public class QuadraticResidues {
+public class QuadraticResiduesMod2PowNTest01 {
 	
-	private static final Logger LOG = Logger.getLogger(QuadraticResidues.class);
-
-    /**
-     * Return all quadratic residues modulo m, where m may be even, too.
-     * Computed by brute force.
-     * 
-     * @param m
-     * @return set of quadratic residues modulo m, sorted bottom up.
-     */
-    public static TreeSet<Long> getQuadraticResidues(long m) {
-    	TreeSet<Long> quadraticResidues = new TreeSet<Long>();
-    	for (long k=0; k<=m/2; k++) {
-    		quadraticResidues.add(k*k % m);
-    	}
-    	return quadraticResidues;
-    }
-    
-    /**
-     * Get the quadratic residues of even "k" modulo m.
-     * @param m
-     * @return square residues generated from even k^2 modulo m
-     */
-    public static TreeSet<Long> getEvenQuadraticResidues(long m) {
-    	TreeSet<Long> quadraticResidues = new TreeSet<Long>();
-    	for (long k=0; k<=m/2; k+=2) {
-    		quadraticResidues.add(k*k % m);
-    	}
-    	return quadraticResidues;
-    }
-
+	private static final Logger LOG = Logger.getLogger(QuadraticResiduesMod2PowNTest01.class);
+	
 	/**
 	 * Test.
 	 * @param args ignored
 	 */
 	public static void main(String[] args) {
 		ConfigUtil.initProject();
-		TreeSet<Long> quadraticResiduesMod100 = getQuadraticResidues(100);
+		TreeSet<Long> quadraticResiduesMod100 = QuadraticResidues.getQuadraticResidues(100);
 		LOG.info("m = 100 has " + quadraticResiduesMod100.size() + " quadratic residues: " + quadraticResiduesMod100);
 		
 		ArrayList<Integer> counts = new ArrayList<Integer>();
@@ -72,11 +44,11 @@ public class QuadraticResidues {
 		for (int n=0; n<20; n++) {
 			int m = 1<<n;
 			
-			TreeSet<Long> quadraticResiduesMod2PowN = getQuadraticResidues(m);
+			TreeSet<Long> quadraticResiduesMod2PowN = QuadraticResidues.getQuadraticResidues(m);
 			LOG.info("m = " + m + " has " + quadraticResiduesMod2PowN.size() + " quadratic residues: " + quadraticResiduesMod2PowN);
 			counts.add(quadraticResiduesMod2PowN.size());
 
-			TreeSet<Long> evenQuadraticResiduesMod2PowN = getEvenQuadraticResidues(m);
+			TreeSet<Long> evenQuadraticResiduesMod2PowN = QuadraticResidues.getEvenQuadraticResidues(m);
 			LOG.info("m = " + m + " has " + evenQuadraticResiduesMod2PowN.size() + " 'even' quadratic residues: " + evenQuadraticResiduesMod2PowN);
 			evenCounts.add(evenQuadraticResiduesMod2PowN.size());
 		}
@@ -86,5 +58,4 @@ public class QuadraticResidues {
 		LOG.info("evenCounts = " + evenCounts);
 		// a(n) = {1, 1} + A023105(n-2) = 1, 1, 1, 2, 2, 3, 4, 7, 12, 23, 44, 87, 172, 343, 684, 1367, 2732, 5463, 10924, 21847
 	}
-
 }
