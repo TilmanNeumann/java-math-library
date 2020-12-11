1b818b3ede25aee252c4196fa43714c56d4a9ad5
Till <Tilman.Neumann@web.de>
2020-12-11 18:49:47 +0100

    unused imports

--- src/de/tilman_neumann/jml/factor/pollardRho/PollardRhoBrentMontgomery64.java
+++ src/de/tilman_neumann/jml/factor/pollardRho/PollardRhoBrentMontgomery64.java
@@ -19,7 +19,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.security.SecureRandom;
 
 import org.apache.log4j.Logger;
 
