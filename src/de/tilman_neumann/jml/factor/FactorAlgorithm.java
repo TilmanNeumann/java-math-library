b10516a6d5c2c1546b51dc667cb6a203b306e76f
Till <Tilman.Neumann@web.de>
2020-12-21 10:02:16 +0100

    keep old factor() implementation for the moment because the new one is still buggy

--- src/de/tilman_neumann/jml/factor/FactorAlgorithm.java
+++ src/de/tilman_neumann/jml/factor/FactorAlgorithm.java
@@ -16,6 +16,7 @@ package de.tilman_neumann.jml.factor;
 import static de.tilman_neumann.jml.base.BigIntConstants.*;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
 
@@ -69,7 +70,93 @@ abstract public class FactorAlgorithm {
 	 * @return The prime factorization of N
 	 */
 	public SortedMultiset<BigInteger> factor(BigInteger N) {
-		boolean searchSmallFactors = true; // XXX should be a method parameter
+		SortedMultiset<BigInteger> primeFactors = new SortedMultiset_BottomUp<BigInteger>();
+		// first get rid of case |N|<=1:
+		if (N.abs().compareTo(I_1)<=0) {
+			// https://oeis.org/wiki/Empty_product#Prime_factorization_of_1:
+			// "the set of prime factors of 1 is the empty set"
+			if (!N.equals(I_1)) {
+				primeFactors.add(N);
+			}
+			return primeFactors;
+		}
+		// make N positive:
+		if (N.signum()<0) {
+			primeFactors.add(I_MINUS_1);
+			N = N.abs();
+		}
+		// Remove multiples of 2:
+		int lsb = N.getLowestSetBit();
+		if (lsb > 0) {
+			primeFactors.add(I_2, lsb);
+			N = N.shiftRight(lsb);
+		}
+		if (N.equals(I_1)) {
+			// N was a power of 2
+			return primeFactors;
+		}
+
+		int Nbits = N.bitLength();
+		if (Nbits > 62) {
+			// "Small" algorithms like trial division, Lehman or Pollard-Rho are very good themselves
+			// at finding small factors, but for larger N we do some trial division.
+			// This will help "big" algorithms to factor smooth numbers much faster.
+			int actualTdivLimit;
+			if (tdivLimit != null) {
+				// use "dictated" limit
+				actualTdivLimit = tdivLimit.intValue();
+			} else {
+				// adjust tdivLimit=2^e by experimental results
+				final double e = 10 + (Nbits-45)*0.07407407407; // constant 0.07.. = 10/135
+				actualTdivLimit = (int) Math.min(1<<20, Math.pow(2, e)); // upper bound 2^20
+			}
+
+			N = tdiv.findSmallOddFactors(N, actualTdivLimit, primeFactors);
+			// TODO add tdiv duration to final report
+			
+			if (N.equals(I_1)) {
+				// N was "easy"
+				return primeFactors;
+			}
+		}
+		
+		// N contains larger factors...
+		ArrayList<BigInteger> untestedFactors = new ArrayList<BigInteger>(); // faster than SortedMultiset
+		untestedFactors.add(N);
+		while (untestedFactors.size()>0) {
+			N = untestedFactors.remove(untestedFactors.size()-1);
+			if (bpsw.isProbablePrime(N)) { // TODO exploit tdiv done so far
+				// N is probable prime. In exceptional cases this prediction may be wrong and N composite
+				// -> then we would falsely predict N to be prime. BPSW is known to be exact for N <= 64 bit.
+				//LOG.debug(N + " is probable prime.");
+				primeFactors.add(N);
+				continue;
+			}
+			BigInteger factor1 = findSingleFactor(N);
+			if (factor1.compareTo(I_1) > 0 && factor1.compareTo(N) < 0) {
+				// found factor
+				untestedFactors.add(factor1);
+				untestedFactors.add(N.divide(factor1));
+			} else {
+				// findSingleFactor() failed to find a factor of the composite N
+				if (DEBUG) LOG.error("Factor algorithm " + getName() + " failed to find a factor of composite " + N);
+				primeFactors.add(N);
+			}
+
+		}
+		//LOG.debug(this.factorAlg + ": => all factors = " + primeFactors);
+		return primeFactors;
+	}
+
+	/**
+	 * Decomposes the argument N into prime factors.
+	 * The result is a multiset of BigIntegers, sorted bottom-up.
+	 * @param N Number to factor.
+	 * @param searchSmallFactors if true then the algorithm may search for small factors before the real algorithm is run
+	 * @return The prime factorization of N
+	 */
+	// XXX still buggy
+	public SortedMultiset<BigInteger> factor(BigInteger N, boolean searchSmallFactors) {
 		
 		SortedMultiset<BigInteger> primeFactors = new SortedMultiset_BottomUp<BigInteger>();
 		// first get rid of case |N|<=1:
