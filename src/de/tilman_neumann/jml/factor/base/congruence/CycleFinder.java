684628aa0e54adc208d8ad26264222aa16b83889
Till <Tilman.Neumann@web.de>
2021-01-27 20:21:10 +0100

    renamed class and some variables +++ improved comments

--- src/de/tilman_neumann/jml/factor/base/congruence/CycleFinder.java
+++ src/de/tilman_neumann/jml/factor/base/congruence/CycleFinder.java
@@ -15,8 +15,7 @@ import de.tilman_neumann.util.SortedMultiset;
 import de.tilman_neumann.util.SortedMultiset_BottomUp;
 
 /**
- * Counts the number of independent cycles in the partial relations.
- * So far it seems to work for 1- and 2-partials.
+ * Algorithms to count and find independent cycles in the partial relations.
  * 
  * @see [LM94] Lenstra, Manasse 1994: "Factoring With Two Large Primes", Mathematics of Computation, volume 63, number 208, page 789.
  * @see [LLDMW02] Leyland, Lenstra, Dodson, Muffett, Wagstaff 2002: "MPQS with three large primes", Lecture Notes in Computer Science, 2369.
@@ -25,13 +24,13 @@ import de.tilman_neumann.util.SortedMultiset_BottomUp;
  */
 // TODO implement cycle-counting for 3-partials
 // TODO implement cycle-finding following [LM94] for 2-partials
-public class CycleCounter {
-	private static final Logger LOG = Logger.getLogger(CycleCounter.class);
+public class CycleFinder {
+	private static final Logger LOG = Logger.getLogger(CycleFinder.class);
 	private static final boolean DEBUG = false; // used for logs and asserts
 
 	// cycle counting
 	private int maxLargeFactors;
-	private HashMap<Long, Long> vertexMap; // contains edges: bigger to smaller prime; size is v = #vertices
+	private HashMap<Long, Long> edges; // contains edges: bigger to smaller prime; size is v = #vertices
 	private HashSet<Long> roots; // roots of disjoint compounds
 	private HashSet<Partial> relations; // all distinct relations
 	private int edgeCount;
@@ -40,9 +39,9 @@ public class CycleCounter {
 	 * Full constructor.
 	 * @param maxLargeFactors the maximum number of large primes in partials:  1, 2 or 3
 	 */
-	public CycleCounter(int maxLargeFactors) {
+	public CycleFinder(int maxLargeFactors) {
 		this.maxLargeFactors = maxLargeFactors;
-		vertexMap = new HashMap<>(); // bigger to smaller prime
+		edges = new HashMap<>(); // bigger to smaller prime
 		roots = new HashSet<>();
 		relations = new HashSet<>();
 		edgeCount = 0;
@@ -79,13 +78,13 @@ public class CycleCounter {
 		// add vertices
 		for (int i=0; i<maxLargeFactors; i++) {
 			long largeFactor = largeFactors[i];
-			if (vertexMap.get(largeFactor) == null) {
+			if (edges.get(largeFactor) == null) {
 				// new vertex creates new compound
-				vertexMap.put(largeFactor, largeFactor); // v = v + 1
+				edges.put(largeFactor, largeFactor); // v = v + 1
 				roots.add(largeFactor); // c = c + 1
 			}
 		}
-		if (DEBUG) LOG.debug("after adding vertices: vertexMap = " + vertexMap + ", roots = " + roots);
+		if (DEBUG) LOG.debug("after adding vertices: edges = " + edges + ", roots = " + roots);
 		
 		// add edges
 		for (int i=0; i<maxLargeFactors; i++) {
@@ -99,14 +98,14 @@ public class CycleCounter {
 					if (DEBUG) LOG.debug("i="+i+", j=" + j + ": f1=" + f1 + ", f2=" + f2 + ", r1=" + r1 + ", r2=" + r2);
 					// insert edge: the smaller root is made the parent of the larger root, and the larger root is no root anymore
 					if (r1 < r2) {
-						vertexMap.put(r2, r1);
+						edges.put(r2, r1);
 						roots.remove(r2);
 					} else if (r1 > r2) {
-						vertexMap.put(r1, r2);
+						edges.put(r1, r2);
 						roots.remove(r1);
 					} // else: r1 and r2 are in the same compound -> a cycle has been found
 					
-					// To speed up the process we could also set the "parents" of all vertexMap nodes passed in root finding to the new root
+					// To speed up the process we could also set the "parents" of all edges nodes passed in root finding to the new root
 				}
 				edgeCount++;
 			}
@@ -119,7 +118,7 @@ public class CycleCounter {
 			
 			LOG.debug(edgeCount + " edges");
 			LOG.debug(roots.size() + " roots = " + roots);
-			LOG.debug(vertexMap.size() + " vertices = " + vertexMap);
+			LOG.debug(edges.size() + " vertices = " + edges);
 			LOG.debug(relations.size() + " relations");
 		}
 	}
@@ -132,9 +131,9 @@ public class CycleCounter {
 	private Long getRoot(long p) {
 		long q;
 		// Not permitting 1 as a root promised finding more smooths, but unfortunately it seems to be wrong
-		//while ((q = vertexMap.get(p)) != p && q!=1) p = q;
+		//while ((q = edges.get(p)) != p && q!=1) p = q;
 		// The following works
-		while ((q = vertexMap.get(p)) != p) p = q;
+		while ((q = edges.get(p)) != p) p = q;
 		return p;
 	}
 
@@ -143,12 +142,12 @@ public class CycleCounter {
 			switch (maxLargeFactors) {
 			case 1:
 			case 2:
-				cycleCount = edgeCount + roots.size() - vertexMap.size();
-				return "maxLargeFactors=" + maxLargeFactors + ": #independent cycles = " + cycleCount + " (" + edgeCount + " edges + " + roots.size() + " compounds - " + vertexMap.size() + " vertices)";
+				cycleCount = edgeCount + roots.size() - edges.size();
+				return "maxLargeFactors=" + maxLargeFactors + ": #independent cycles = " + cycleCount + " (" + edgeCount + " edges + " + roots.size() + " compounds - " + edges.size() + " vertices)";
 			case 3:
-				// The "thought to be" formula from [Leyland, Lenstra, Dodson, Muffett, Wagstaff: "MPQS with three large primes", Lecture Notes in Computer Science, 2369, p.7]
-				cycleCount = edgeCount + roots.size() - vertexMap.size() - 2*relations.size();
-				return "maxLargeFactors=" + maxLargeFactors + ": #independent cycles = " + cycleCount + " (" + edgeCount + " edges + " + roots.size() + " compounds - " + vertexMap.size() + " vertices - 2*" + relations.size() + " relations)";
+				// The "thought to be" formula from [LLDMW02], p.7
+				cycleCount = edgeCount + roots.size() - edges.size() - 2*relations.size();
+				return "maxLargeFactors=" + maxLargeFactors + ": #independent cycles = " + cycleCount + " (" + edgeCount + " edges + " + roots.size() + " compounds - " + edges.size() + " vertices - 2*" + relations.size() + " relations)";
 			default:
 				throw new IllegalStateException("cycle counting is not implemented yet for maxLargeFactors>3, but maxLargeFactors = " + maxLargeFactors);
 			}
@@ -159,26 +158,26 @@ public class CycleCounter {
 	 * Finds independent cycles and uses them to combine partial to smooth relations, following [LLDMW02].
 	 * Tested with 2 large primes, and should work for 3 large primes, too.
 	 */
-	public void findCycles() {
+	public void findIndependentCycles() {
 		// Create maps from large primes to partials, vice versa, and chains.
 		// These are needed so we can remove elements without changing the relations itself.
-		HashMap<Long, ArrayList<Partial>> largeFactors_2_partials = new HashMap<>();
-		HashMap<Partial, ArrayList<Long>> partials_2_largeFactors = new HashMap<>();
+		HashMap<Long, ArrayList<Partial>> rbp = new HashMap<>();
+		HashMap<Partial, ArrayList<Long>> pbr = new HashMap<>();
 		HashMap<Partial, ArrayList<Partial>> chains = new HashMap<>();
 		for (Partial newPartial : relations) {
 			Long[] oddExpBigFactors = newPartial.getLargeFactorsWithOddExponent();
 			ArrayList<Long> factorsList = new ArrayList<>(); // copy needed
 			for (Long oddExpBigFactor : oddExpBigFactors) {
 				factorsList.add(oddExpBigFactor);				
-				ArrayList<Partial> partialCongruenceList = largeFactors_2_partials.get(oddExpBigFactor);
+				ArrayList<Partial> partialCongruenceList = rbp.get(oddExpBigFactor);
 				// For large N, most large factors appear only once. Therefore we create an ArrayList with initialCapacity=1 to safe memory.
 				// Even less memory would be needed if we had a HashMap<Long, Object>
 				// and store AQPairs or AQPair[] in the Object part. But I do not want to break the generics...
 				if (partialCongruenceList==null) partialCongruenceList = new ArrayList<Partial>(1);
 				partialCongruenceList.add(newPartial);
-				largeFactors_2_partials.put(oddExpBigFactor, partialCongruenceList);
+				rbp.put(oddExpBigFactor, partialCongruenceList);
 			}
-			partials_2_largeFactors.put(newPartial, factorsList);
+			pbr.put(newPartial, factorsList);
 			chains.put(newPartial, new ArrayList<>());
 		}
 		
@@ -188,18 +187,18 @@ public class CycleCounter {
 		boolean tablesChanged;
 		do {
 			tablesChanged = false;
-			Iterator<Partial> r0Iter = partials_2_largeFactors.keySet().iterator();
+			Iterator<Partial> r0Iter = pbr.keySet().iterator();
 			while (r0Iter.hasNext()) {
 				Partial r0 = r0Iter.next();
-				ArrayList<Long> r0Factors = partials_2_largeFactors.get(r0);
+				ArrayList<Long> r0Factors = pbr.get(r0);
 				if (r0Factors.size() != 1) continue;
 				
 				Long p = r0Factors.get(0);
-				ArrayList<Partial> riList = largeFactors_2_partials.get(p);
+				ArrayList<Partial> riList = rbp.get(p);
 				for (Partial ri : riList) {
 					if (r0.equals(ri)) continue;
 					
-					ArrayList<Long> riFactors = partials_2_largeFactors.get(ri);
+					ArrayList<Long> riFactors = pbr.get(ri);
 					if (DEBUG) assertNotNull(riFactors);
 					if (riFactors.size() == 1) {
 						// found cycle -> create new Smooth consisting of r0, ri and their chains
@@ -232,17 +231,17 @@ public class CycleCounter {
 					if (riFactors != null) riFactors.remove(p);
 				} // end for ri
 
-				// "the entry keyed by r0 is deleted from pbr", pbr = partials_2_largeFactors;
-				// this requires iterator.remove() so we can continue working with the outer collection
-				r0Iter.remove(); // except avoiding ConcurrentModificationExceptions the same as partials_2_largeFactors.remove(r0);
+				// "the entry keyed by r0 is deleted from pbr";
+				// this requires Iterator.remove() so we can continue working with the outer collection
+				r0Iter.remove(); // except avoiding ConcurrentModificationExceptions the same as pbr.remove(r0);
 				
-				// "the entry for r0 keyed by p is deleted from rbp", rbp = largeFactors_2_partials
+				// "the entry for r0 keyed by p is deleted from rbp";
 				// This choice promised finding more smooths, but unfortunately it was wrong, delivered combinations with odd exponents
 				// riList.remove(r0);
 				// The following works
-				ArrayList<Partial> partials = largeFactors_2_partials.get(p);
+				ArrayList<Partial> partials = rbp.get(p);
 				for (Partial partial : partials) {
-					ArrayList<Long> pList = partials_2_largeFactors.get(partial);
+					ArrayList<Long> pList = pbr.get(partial);
 					if (pList != null) pList.remove(p);
 				}
 				
