6ad67e19527f257c054b2ac2bb2e9c91878caa6a
Till <Tilman.Neumann@web.de>
2021-05-06 19:49:40 +0200

    renamed single-threaded Gaussian solvers

--- src/de/tilman_neumann/jml/factor/base/matrixSolver/MatrixSolver_Gauss03.java
+++ src/de/tilman_neumann/jml/factor/base/matrixSolver/MatrixSolver_Gauss03.java
@@ -25,19 +25,21 @@ import de.tilman_neumann.jml.factor.base.congruence.AQPair;
 import de.tilman_neumann.jml.factor.base.congruence.Smooth;
 
 /**
- * A simple congruence equation system solver, doing Gaussian elimination.
- * Much faster than the first version due to improvements by Dave McGuigan.
- * Best single-threaded Gaussian solver for N<=250 bit so far.
+ * Best single-threaded Gaussian solver for N > 250 bit.
  * 
- * @author Tilman Neumann, Dave McGuigan
+ * The main improvement came from adopting the new base class due to Dave McGuigan.
+ * Another push for large N was a selection of pivot rows leading to sparser matrices during the solution procedure;
+ * this required to take explicit control of the biggest column indices of all rows.
+ * 
+ * @author Tilman Neumann
  */
-public class MatrixSolver_Gauss01b extends MatrixSolverBase02 {
+public class MatrixSolver_Gauss03 extends MatrixSolverBase02 {
 	@SuppressWarnings("unused")
-	private static final Logger LOG = Logger.getLogger(MatrixSolver_Gauss01b.class);
+	private static final Logger LOG = Logger.getLogger(MatrixSolver_Gauss03.class);
 	
 	@Override
 	public String getName() {
-		return "GaussSolver01b";
+		return "GaussSolver03";
 	}
 
 	@Override
@@ -47,38 +49,61 @@ public class MatrixSolver_Gauss01b extends MatrixSolverBase02 {
 
 	@Override
 	protected void solve(List<Smooth> congruences, Map<Integer, Integer> factors_2_columnIndices) throws FactorException {
+		
 		// create matrix
 		List<MatrixRow> rows = createMatrix(congruences, factors_2_columnIndices);
+		
+		// create sorted map from biggest columns to row indices (using an array having all keys seems to be faster for small N than TreeMap)
+		int columnCount = factors_2_columnIndices.size();
+		@SuppressWarnings("unchecked")
+		ArrayList<Integer>[] biggestColumnIndex2RowIndices = new ArrayList[columnCount];
+		for (int i=columnCount-1; i>=0; i--) {
+			biggestColumnIndex2RowIndices[i] = new ArrayList<Integer>();
+		}
+		for (int rowIndex=0; rowIndex<rows.size(); rowIndex++) {
+			MatrixRow row = rows.get(rowIndex);
+			int biggestColumnIndex = row.getBiggestColumnIndex();
+			List<Integer> rowIndices = biggestColumnIndex2RowIndices[biggestColumnIndex];
+			rowIndices.add(rowIndex);
+		}
+		
 		// solve
-		MatrixRow[] pivotRowsForColumns = new MatrixRow[factors_2_columnIndices.size()]; // storage for the pivot rows as they are found
-		for(MatrixRow row : rows) {
-			int columnIndex = row.getBiggestColumnIndex();
-			while(columnIndex >= 0) {
-				MatrixRow pivot = pivotRowsForColumns[columnIndex];
-				if(pivot == null) {
-					pivotRowsForColumns[columnIndex] = row;
-					break;
-				}
-				
-				// solution operations taken directly from MatrixSolver_Gauss01 ++
-				row.addXor(pivot); // This operation should be fast!
-				if (row.isNullVector()) {
-					//LOG.debug("solve(): 5: Found null-vector: " + row);
-					// Found null vector -> recover the set of AQ-pairs from its row index history
-					HashSet<AQPair> totalAQPairs = new HashSet<AQPair>(); // Set required for the "xor"-operation below
-					for (int rowIndex : row.getRowIndexHistoryAsList()) {
-						Smooth congruence = congruences.get(rowIndex);
-						// add the new AQ-pairs via "xor"
-						congruence.addMyAQPairsViaXor(totalAQPairs);
+		for (int pivotColumnIndex=columnCount-1; pivotColumnIndex>=0; pivotColumnIndex--) {
+			// Find pivot column index and row:
+			List<Integer> rowIndices = biggestColumnIndex2RowIndices[pivotColumnIndex];
+			if (rowIndices.size() > 0) {
+				int pivotRowIndex = selectPivotRowIndex(rowIndices, rows);
+				rowIndices.remove(Integer.valueOf(pivotRowIndex));
+				MatrixRow pivotRow = rows.get(pivotRowIndex);
+	
+				// Do one Gaussian elimination step
+				for (int rowIndex : rowIndices) {
+					MatrixRow row = rows.get(rowIndex);
+					// Add the pivot row to the current row in Z_2 ("xor"):
+					// We can modify the current row object because its old state is not required anymore,
+					// and because working on it does not affect the original congruences.
+					row.addXor(pivotRow); // This operation should be fast!
+					
+					// update biggest column index map
+					int updatedBiggestColumnIndex = row.getBiggestColumnIndex();
+					if (updatedBiggestColumnIndex >= 0) {
+						List<Integer> updatedRowIndices = biggestColumnIndex2RowIndices[updatedBiggestColumnIndex];
+						updatedRowIndices.add(rowIndex);
 					}
-					// "return" the AQ-pairs of the null vector
-					processNullVector(totalAQPairs);
-					break;
-				} else {
-					// else: current row is not a null-vector, keep trying to reduce
-					columnIndex = row.getBiggestColumnIndex();
+					
+					if (row.isNullVector()) {
+						// Found null vector -> recover the set of AQ-pairs from its row index history
+						HashSet<AQPair> totalAQPairs = new HashSet<AQPair>(); // Set required for the "xor"-operation below
+						for (int rowIndex2 : row.getRowIndexHistoryAsList()) {
+							Smooth congruence = congruences.get(rowIndex2);
+							// add the new AQ-pairs via "xor"
+							congruence.addMyAQPairsViaXor(totalAQPairs);
+						}
+						// "return" the AQ-pairs of the null vector
+						processNullVector(totalAQPairs);
+					}
 				}
-			}	
+			}
 		}
 	}
 
@@ -124,5 +149,25 @@ public class MatrixSolver_Gauss01b extends MatrixSolverBase02 {
 		rowIndexHistory.add(rowIndex);
 		//LOG.debug("numberOfRows=" + numberOfRows + ", rowIndex=" + rowIndex + " -> rowIndexHistory = " + rowIndexHistory);
 		return rowIndexHistory;
+	}
+	
+	/**
+	 * Return the index of the row to become the next pivot row.
+	 * @param choosableRowIndices
+	 * @param rows
+	 * @return index of the chosen row
+	 */
+	private int selectPivotRowIndex(List<Integer> choosableRowIndices, List<MatrixRow> rows) {
+		int minWeight = Integer.MAX_VALUE;
+		int minRowIndex = -1;
+		for (int rowIndex : choosableRowIndices) {
+			MatrixRow row = rows.get(rowIndex);
+			int weight = row.getColumnCount();
+			if (weight < minWeight) {
+				minWeight = weight;
+				minRowIndex = rowIndex;
+			}
+		}
+		return minRowIndex;
 	}
 }
