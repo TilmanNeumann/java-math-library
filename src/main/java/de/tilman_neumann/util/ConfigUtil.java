ff9b1c604b7410ece603a7666eeda22a86204518
Till <Tilman.Neumann@web.de>
2024-12-25 16:33:45 +0100

    add build for runnable jar, and improve log4j2 init for that

--- src/main/java/de/tilman_neumann/util/ConfigUtil.java
+++ src/main/java/de/tilman_neumann/util/ConfigUtil.java
@@ -1,6 +1,6 @@
 /*
  * java-math-library is a Java library focused on number theory, but not necessarily limited to it. It is based on the PSIQS 4.0 factoring project.
- * Copyright (C) 2018 Tilman Neumann - tilman.neumann@web.de
+ * Copyright (C) 2018-2024 Tilman Neumann - tilman.neumann@web.de
  *
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
@@ -15,7 +15,6 @@ package de.tilman_neumann.util;
 
 import java.io.File;
 import java.net.URL;
-import java.util.Objects;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
@@ -35,29 +34,46 @@ import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
  */
 public class ConfigUtil {
 
+	@SuppressWarnings("unused")
 	private static final Logger LOG = LogManager.getLogger(ConfigUtil.class);
 	
 	private static boolean initialized = false;
 	
 	/** File separator used on this OS. */
 	public static String FILE_SEPARATOR;
+	
 	/** Path separator used on this OS. */
 	public static String PATH_SEPARATOR;
-	/** The root directory of the current project. */
+	
+	/**
+	 * The root directory of the current project, derived from the system property 'user.dir'. This is<br>
+	 * a) the jml project root directory if jml is run as a project<br>
+	 * b) the project root directory of a custom project using jml as a jar<br>
+	 * c) the folder containing the jar file if jml is run as a runnable jar<br>
+	 */
 	public static String PROJECT_ROOT;
-	/** The base folder for all configuration files in this project. */
+	
+	/** The folder for configuration files in this project. */
 	public static String CONF_ROOT;
+	
 	/** Java class path */
 	public static String JAVA_CLASS_PATH;
+	
 	/** Java library path */
 	public static String JAVA_LIBRARY_PATH;
+	
 	/** Java temp directory */
 	public static String JAVA_TMP_DIR;
+	
 	/** user home directory */
 	public static String USER_HOME;
-
+	
 	/** number of processors to use for parallel implementations */
 	public static int NUMBER_OF_PROCESSORS;
+
+	/** if true, then jml is deployed as a (possibly executable) jar file; otherwise as a Java project. */
+	public static boolean IS_DEPLOYED_AS_JAR;
+	
 	
 	private ConfigUtil() {
 		// static class
@@ -83,11 +99,6 @@ public class ConfigUtil {
 		if (verbose) System.out.println("system file separator = " + FILE_SEPARATOR);
 		PATH_SEPARATOR = System.getProperty("path.separator");
 		if (verbose) System.out.println("system path separator = " + PATH_SEPARATOR);
-		// user.dir is
-		// * the jml project root directory if jml is run as a project
-		// * the project root directory of a custom project using jml as a jar
-		// * the folder containing the jar file if jml is run as a runnable jar
-		// So this is a good place to put the data and configuration files required by jml
 		PROJECT_ROOT = System.getProperty("user.dir");
 		if (verbose) System.out.println("project root directory (user.dir) = " + PROJECT_ROOT);
 		CONF_ROOT = PROJECT_ROOT + FILE_SEPARATOR + "conf";
@@ -102,48 +113,12 @@ public class ConfigUtil {
 		if (verbose) System.out.println("user.home = " + USER_HOME);
 		NUMBER_OF_PROCESSORS = Runtime.getRuntime().availableProcessors();
 		if (verbose) System.out.println("number of processors = " + NUMBER_OF_PROCESSORS);
+		IS_DEPLOYED_AS_JAR = "jar".equals(getClassResourceProtocol(verbose));
+		if (verbose) System.out.println("jml deployed as jar = " + IS_DEPLOYED_AS_JAR);
 		
-		String confFileStr = CONF_ROOT + FILE_SEPARATOR + "log4j2-test.xml";
-		File confFile = new File(confFileStr);
-		if (confFile.exists()) {
-			// initialize Log4j from xml configuration
-	    	//DOMConfigurator.configure(confFileStr);
-			System.setProperty("log4j.configurationFile", confFileStr);
-			Configurator.reconfigure(); // TODO do we need this?
-	    	if (verbose) LOG.info("log4j configuration successfully loaded from file " + confFileStr + ".");
-	    	if (verbose) LOG.info("project initialization finished.");
-	    	initialized = true;
-	    	return;
-		}
-		
-		// Not finding the log4j config file would be bad when not run from runnable jar
-		boolean runningFromJar = Objects.equals(getClassResourceProtocol(verbose), "rsrc");
-		if (!runningFromJar) {
-			System.err.println("WARNING: Unable to find log4j configuration file " + confFileStr + ".");
-			System.err.println("WARNING: An emergency logger with limited capabilities will be used.");
-			System.err.println("WARNING: Please put a proper log4jconf.xml file into the designated folder if this message is annoying you...");
-		}
-		
-		// Create default logger, following https://www.baeldung.com/log4j2-programmatic-config
-		ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
-		// create appenders to console and log.txt
-		AppenderComponentBuilder console = builder.newAppender("stdout", "Console"); 
-		builder.add(console);
-		AppenderComponentBuilder file = builder.newAppender("log", "File"); 
-		file.addAttribute("fileName", "log.txt");
-		builder.add(file);
-		// declare layouts
-		LayoutComponentBuilder standard = builder.newLayout("PatternLayout");
-		standard.addAttribute("pattern", "%d [%t] %-5level: %msg%n%throwable");
-		console.add(standard);
-		file.add(standard);
-		// set up root logger with debug level
-		RootLoggerComponentBuilder rootLogger = builder.newRootLogger(Level.DEBUG);
-		rootLogger.add(builder.newAppenderRef("stdout"));
-		builder.add(rootLogger);
-		// let it be used
-		Configurator.initialize(builder.build());
-		LOG.info("Created default logger:\n" + builder.toXmlConfiguration());
+		setupLog4j2(verbose);
+	    
+    	if (verbose) System.out.println("project initialization finished.");
     	initialized = true;
 	}
 	
@@ -156,4 +131,102 @@ public class ConfigUtil {
 		if (verbose) System.out.println("protocol = " + protocol);
 		return protocol;
 	}
+	
+	private static void setupLog4j2(boolean verbose) {
+		// Set up log4j2...
+		// First choice is any custom configuration file defined by system property LOG4J2_CONFIGURATION_FILE
+		String log4j2ConfigFile = System.getProperty("log4j2.configurationFile");
+		if (log4j2ConfigFile != null) {
+			if (new File(log4j2ConfigFile).exists()) {
+				// the logger should already be initialized correctly, but here we write to stdout because we don't know the log level
+				if (verbose) System.out.println("Using custom log4j2 configuration file " + log4j2ConfigFile + " defined by system property 'log4j2.configurationFile'");
+				return;
+			} else {
+				if (verbose) System.out.println("The log4j2 configuration file " + log4j2ConfigFile + " defined by system property 'log4j2.configurationFile' does not exist");
+			}
+		} else {
+			if (verbose) System.out.println("The system property 'log4j2.configurationFile' is not defined.");
+		}
+		
+		if (IS_DEPLOYED_AS_JAR) {
+			// So far I failed to find a way to change the log level in the case that jml is run from a jar.
+			// Thus the only possible fallback is the default log4j2 console Logger with loglevel ERROR.
+			// This will be disappointing if it is a runnable jar and we see no input prompt nor factoring outputs...
+			if (verbose) {
+				System.out.println("jml will use the default log4j2 console Logger with loglevel ERROR.");
+				System.out.println("To take control of the logging behavior, define the system property 'log4j2.configurationFile' such that it points to a valid log4j2 configuration file.");
+				System.out.println("For a runnable jar, use something like 'java -Dlog4j2.configurationFile=<some-file-path>/log4j2-test.xml -jar jml<version>.jar'");
+			}
+			return; // initialized, though not perfectly
+		}
+
+		// Now we know that jml is run as a Java project. In that case we have more options...
+		// 2nd choice is the default log4j2-test.xml in the conf folder of the project.
+		String defaultConfigFile = CONF_ROOT + FILE_SEPARATOR + "log4j2-test.xml";
+		if (new File(defaultConfigFile).exists()) {
+			// initialize Log4j2 from xml configuration
+			System.setProperty("log4j2.configurationFile", defaultConfigFile);
+			Configurator.reconfigure();
+	    	if (verbose) System.out.println("log4j configuration successfully loaded from file " + defaultConfigFile + ".");
+	    	return;
+		} else {
+			if (verbose) {
+				System.out.println("The default configuration file " + defaultConfigFile + " does not exist");
+				System.out.println("An emergency logger will be used that logs to console only.");
+			}
+		}
+		
+		// 3rd choice is to setup a simple emergencey logger.
+		// For this we could use the log4j2 default logger
+		//configureDefaultLogger_v1();
+		// or create one programmatically on our own
+		configureDefaultLogger_v2(false);
+	}
+	
+	/**
+	 * This version simply changes the log level of the log4j2 default logger.
+	 * The default logger only logs to console and has pattern layout "%d [%t] %-5p %c - %m%n".
+	 * 
+	 * Works only when run as a project.
+	 */
+	private static void configureDefaultLogger_v1() {
+		Configurator.setRootLevel(Level.INFO);
+	}
+
+	/**
+	 * Create an emergency logger on our own.
+	 * 
+	 * Works only when run as a project.
+	 * 
+	 * @param addFileLogger
+	 */
+	private static void configureDefaultLogger_v2(boolean addFileLogger) {
+		// Create default logger, following https://www.baeldung.com/log4j2-programmatic-config.
+		// This works only when running from a project.
+		ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
+		// create console appenders
+		AppenderComponentBuilder console = builder.newAppender("stdout", "Console"); 
+		builder.add(console);
+		// add layout
+		LayoutComponentBuilder consoleLayout = builder.newLayout("PatternLayout");
+		consoleLayout.addAttribute("pattern", "%d %-5p %c{1}(%L) [%t]: %m%n");
+		console.add(consoleLayout);
+
+		if (addFileLogger) {
+			AppenderComponentBuilder file = builder.newAppender("log", "File"); 
+			file.addAttribute("fileName", "log.txt");
+			builder.add(file);
+			LayoutComponentBuilder fileLayout = builder.newLayout("PatternLayout");
+			fileLayout.addAttribute("pattern", "%m%n");
+			file.add(fileLayout);
+		}
+		
+		// set up root logger
+ 		RootLoggerComponentBuilder rootLogger = builder.newRootLogger(Level.DEBUG);
+		rootLogger.add(builder.newAppenderRef("stdout"));
+		builder.add(rootLogger);
+		// let it be used
+		Configurator.reconfigure(builder.build());		
+	}
+	
 }
